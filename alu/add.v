e6667be4fe6eebf1614ab15944783678642c3df5
Ayush Tiwari <ayushtiwari@icloud.com>
2019-12-04 00:43:53 +0530

    final

---
+++ alu/add.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:26:28 10/30/2019 
+// Design Name: 
+// Module Name:    Add 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:34:31 10/30/2019 
+// Design Name: 
+// Module Name:    add 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Add(
+    input [3:0] A,
+    input [3:0] B,
+    input cin,
+    output [3:0] S,
+    output cout
+    );
+    
+    wire [3:0] P;
+    wire [3:0] C;
+    wire [3:0] G;
+    
+    assign C[0] = cin;
+    assign P = A^B;
+    assign G = A&B;
+        
+    assign C[1] = G[0] | (P[0]&C[0]);
+    assign C[2] = G[1] | (P[1]&G[0]) | (P[1]&P[0]&C[0]);
+    assign C[3] = G[2] | (P[2]&G[1]) | (P[2]&P[1]&G[0]) | (P[2]&P[1]&P[0]&C[0]);
+    assign cout = G[3] | (P[3]&G[2]) | (P[3]&P[2]&G[1]) | (P[3]&P[2]&P[1]&G[0]) | (P[3]&P[2]&P[1]&P[0]&C[0]);
+    
+    assign S = P^C;
+
+endmodule
